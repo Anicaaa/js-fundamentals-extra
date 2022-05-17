8cc7396e70bb1638c6419a07bb3ba7754c130e92
Anica Rahman <anicarahman2009@yahoo.it>
2022-05-17 10:15:45 +0100

    First exercise

--- src/js-fundamentals-extra.js
+++ src/js-fundamentals-extra.js
@@ -1,14 +1,22 @@
 // SecondsInHours
-//
-// Create a function that takes a number of hours and returns the number of 
-// seconds in those hours. 
-//
+
+// Create a function that takes a number of hours and returns the number of
+// seconds in those hours.
+
 // Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
-//
+
 // TODO: write code below
 
+// Comments:
+// 1. Find how many minutes are in an hour.
+// 2. Count how many seconds are in those minutes.
 
+function secondsInHours(hours) {
+  let seconds = hours * 60 * 60;
+  return seconds;
+}
+console.log(secondsInHours(7));
 
 // MilesTravelled
 //
@@ -18,42 +26,36 @@
 // perform the rounding. You can assume only whole numbers will be provided
 // to the function.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
 
 // KilometersToMiles
 //
-// Create a function that takes a number of kilometers and converts it to miles. 
-// For this task assume there are exactly 1.6 kilometers in a mile. The returned 
+// Create a function that takes a number of kilometers and converts it to miles.
+// For this task assume there are exactly 1.6 kilometers in a mile. The returned
 // mile distance should be rounded to the nearest mile. You can  use the Math.round
 // function to do this.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
 
-
-
 // MakeSentence
 //
-// Create a function that takes a string and returns the same string with the 
-// first letter capitalized and a full stop added on to the end. If the string 
-// already ends with a full stop, question mark (?) or an exclamation mark(!) 
+// Create a function that takes a string and returns the same string with the
+// first letter capitalized and a full stop added on to the end. If the string
+// already ends with a full stop, question mark (?) or an exclamation mark(!)
 // then a full stop should not be added.
 //
 // You may need to the use `String.substring` method.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
 
 // FileExtension
 //
@@ -62,73 +64,65 @@
 // string should be returned. You may need to the use `String.substring` method and
 // the `String.lastIndexOf` method.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
 
 // Range
 //
-// Create a function that takes an array of numbers and returns the difference 
+// Create a function that takes an array of numbers and returns the difference
 // between the highest and lowest numbers in the array. You can assume the array
 // will always contain at least 1 number (it will never be empty)
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
 
-
-
 // CheckTransactions
 //
-// Create a function that checks if a list of banking transactions is valid for 
-// a given account. 
+// Create a function that checks if a list of banking transactions is valid for
+// a given account.
 //
-// The function should accept 3 arguments. The first is an array containing a 
-// list of transactions to apply to the account balance. Positive numbers are 
-// deposits and negative numbers are withdrawals. The second argument is the 
+// The function should accept 3 arguments. The first is an array containing a
+// list of transactions to apply to the account balance. Positive numbers are
+// deposits and negative numbers are withdrawals. The second argument is the
 // account starting balance and the third is the account overdraft. The function
 // should return false if applying the list of transactions causes the account
 // balance to go below the overdraft value at any point.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
 
 // FilmsInGenre
 //
 // Create a function that takes an array of film objects, a film genre, and returns
-// an array of film names that match the supplied genre. Each film object contains a 
-// `name` key that contains the film name and a 'genre' key that contains an array 
+// an array of film names that match the supplied genre. Each film object contains a
+// `name` key that contains the film name and a 'genre' key that contains an array
 // of strings describing what genres the film belongs to. See the spec file for
 // example data.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
 
-
-
 // TODO: change undefined to be the name of the functions you defined
 module.exports = {
   //SecondsInHours
-  a: undefined,
+  a: secondsInHours,
 
   //MilesTravelled,
-  b: undefined,  
+  b: undefined,
 
   //KilometersToMiles,
-  c: undefined, 
+  c: undefined,
 
   //MakeSentence
-  d: undefined, 
+  d: undefined,
 
   //FileExtension
   e: undefined,
@@ -141,4 +135,4 @@ module.exports = {
 
   //FilmsInGenre
   h: undefined,
-}
+};
